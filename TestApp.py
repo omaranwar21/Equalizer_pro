2b0c27024d41df50d2759d2b61db293b0e8de5d4
omaranwar21 <omarahmed11185@gmail.com>
2022-11-05 18:44:42 +0200

    Semi-structure1

---
+++ TestApp.py
@@ -0,0 +1,105 @@
+import wave
+from scipy.io import wavfile
+from scipy.io.wavfile import write
+import streamlit as st
+import numpy as np
+import pandas as pd
+import sys
+from utils import *
+import matplotlib.pyplot as plt
+
+import librosa
+
+
+file = st.file_uploader(label="Upload Sound File", key="uploaded_file",type=["wav"])
+if not file:
+    st.stop()
+
+
+f,ax=init_plot() 
+
+# file =
+x, sr =librosa.load(r"C:\\Users\\Anwar\\Desktop\\SBME 2024\YEAR 3 (2022-2023)\\DSP\\Tasks\\Task 2\\DSP_Task2\\Media\\S1.wav")
+
+t=np.array(range(0,len(x)))/(sr)
+f2,cx=init_plot()
+add_to_plot(cx, t, x)
+show_plot(f2)
+
+frequencies, fourier_signal = fourier(x, sr)
+add_to_plot(ax, frequencies[:len(fourier_signal)], fourier_signal)
+show_plot(f)
+
+t=np.array(range(0,len(fourier_signal )))/(sr)
+f1,bx=init_plot()
+inverse = play(fourier_signal)
+add_to_plot(bx, t, inverse)
+
+
+
+# df= pd.read_csv("C:\\Users\\Anwar\\Desktop\\SBME 2024\\YEAR 3 (2022-2023)\\DSP\\Tasks\\Task 2\\DSP_Task2\\Media\\ECG.csv")
+# time = df['time']
+# signal = df['values']
+# t=np.array(range(0,len(x)))/sr
+# plt.figure(figsize=(14,5))
+
+# plt.title("waveform of wave file")
+# # plt.plot(t,x , color="blue")
+# # plt.plot(time,signal , color="blue")
+# plt.xlabel("Time")
+# plt.ylabel("amplitude")
+# # plt.show()
+# add_to_plot(ax,t,x)
+# # add_to_plot(ax,time,signal)
+
+# show_plot(f)
+
+# y= np.fft.fft(x)
+# # y= np.fft.fft(signal)
+g,gx=init_plot() 
+signal_droped = drop(fourier_signal, frequencies, 0, 2000, 0)
+
+add_to_plot(gx, frequencies[:len(fourier_signal)], signal_droped)
+show_plot(g)
+
+singnal_inversed = play(signal_droped)
+
+d,dx=init_plot() 
+add_to_plot(dx, t, singnal_inversed)
+show_plot(f1)
+show_plot(d)
+# tpCount     = len(x)
+# # tpCount     = len(signal)
+
+# values      = np.arange(int(tpCount))
+
+# timePeriod  = tpCount/sr
+# # timePeriod  = time[1] - time[0] 
+
+# frequencies = values/timePeriod
+# # fr=np.array(range(0,len(x)))/4
+# plt.figure(figsize=(10,5))
+# plt.xlabel('freq')
+# condition = ((frequencies>344)& (frequencies<1341))
+
+# y[condition]=y[condition]*15   # 0.1 --> slider value
+
+# add_to_plot(ax, frequencies, y)
+# show_plot(f)
+
+# inverse=np.fft.ifft(y)
+
+
+# add_to_plot(ax,t,inverse)
+
+# show_plot(f)
+
+
+
+
+
+write("openS1.wav", sr, singnal_inversed.astype(np.int16))
+
+st.audio("C:\\Users\\Anwar\\Desktop\\SBME 2024\YEAR 3 (2022-2023)\\DSP\\Tasks\\Task 2\\DSP_Task2\\Media\\S1.wav")
+
+st.audio("openS1.wav")
--- utils.py
+++ utils.py
@@ -4,7 +4,54 @@ import base64
 import streamlit as st
 import pandas as pd
 import wave
+import matplotlib.pyplot as plt
+
+
+#init function to dispaly needed grid
+def init_plot():
+  f, ax = plt.subplots(1,1,figsize=(10,10))                       
+  ax.set_xlabel('Time')              # the x_axis title
+  ax.yaxis.set_tick_params(length=5)          # to draw the y-axis line (-) for points
+  ax.xaxis.set_tick_params(length=0)          # to draw the y-axis line (-) for points
+  ax.grid(c='#D3D3D3', lw=1, ls='--')         #lw represent the line width of the grid
+  legend = ax.legend()      
+  legend.get_frame().set_alpha(1)
+  for spine in ('top', 'right', 'bottom', 'left'):  #control the border lines to be visible or not and the width of them
+    ax.spines[spine].set_visible(False)
+  return  f,ax
+
+#function to pass the x,y data
+def add_to_plot(ax,x,y):
+  ax.plot(x,y,alpha=0.7,linewidth=2)  # alpha represent the brightness of the line
+#fuction to shaw the plotting
+def show_plot(f):
+  st.pyplot(f)   
+
 
 def read_wav(file):
     spf = wave.open(file, "r")
     return spf
+
+
+def fourier(signal, sampleRate):
+    n_samples = len(signal)
+    f_hat = np.fft.fft(signal, n_samples)
+    l = (len(f_hat))
+    f = f_hat[:l]
+    tpCount     = len(signal)
+    values      = np.arange(int(tpCount))
+    timePeriod  = tpCount/sampleRate 
+    frequencies = values/timePeriod
+    return frequencies, f 
+
+
+def play(signal):
+    inversed_signal = np.fft.ifft(signal)
+    return inversed_signal
+
+
+def drop (signal, frequencies, low, high, ratio):
+    condition = ((frequencies>low)& (frequencies<high))
+    signal[condition]=signal[condition]*ratio   # 0.1 --> slider value
+    return signal
+
